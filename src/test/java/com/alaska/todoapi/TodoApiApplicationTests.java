17a5544e05f5d683f427f2949f714a5c368eaecf
Stephen Adom Addae <stephen.alaska94@hotmail.com>
2023-09-21 15:29:19 +0000

    initialize spring boot project

---
+++ src/test/java/com/alaska/todoapi/TodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alaska.todoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
